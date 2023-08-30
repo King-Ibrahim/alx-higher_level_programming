023dd8b62b1d96b70a0b385d4a844ee33a8c92fd
King-Ibrahim <mdotibrahim@gmail.com>
2023-08-30 14:29:24 -0700

    Almost done with 101

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+class Square:
+    """Empty class that defining a square"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+class Square:
+    """Define a square"""
+    def __init__(self, size):
+        """Initialise the data"""
+        self.__size = size
